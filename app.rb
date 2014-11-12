6f74eff0ce7b2dec299df5372da0b3a5436dcbd4
Jackson Chung <cywjackson@users.noreply.github.com>
2014-11-11 16:19:22 -0800

    Update app.rb

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ set :port,8080
 get '/' do
   "Hello World!\n"+
 # ENV values are generated during template processing
+# done
 # and then passed to the container when openshift launches it.
   "User is #{ENV['ADMIN_USERNAME']}\n"+
   "Password is #{ENV['ADMIN_PASSWORD']}\n"+
